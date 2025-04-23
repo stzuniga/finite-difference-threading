de48ce42fe4c92c77a922b441e2f3723b4843ffd
stzuniga <stzuniga@unm.edu>
2025-04-23 12:30:10 -0600

    Corrected paths on timingThreadsPlot,py

--- code/timingThreadsPlot.py
+++ code/timingThreadsPlot.py
@@ -15,9 +15,9 @@ def load_timings(filename):
     with open(filename, 'r') as file:
         return [float(value) for value in file.read().split()]
 
-timings_1_thread = load_timings('/Users/stevenzuniga/Desktop/cs471/2024-fall-471-zunigasteven/hw4/code/timings1thread.txt')
-timings_2_threads = load_timings('/Users/stevenzuniga/Desktop/cs471/2024-fall-471-zunigasteven/hw4/code/timings2thread.txt')
-timings_3_threads = load_timings('/Users/stevenzuniga/Desktop/cs471/2024-fall-471-zunigasteven/hw4/code/timings3thread.txt')
+timings_1_thread = load_timings('/Users/stevenzuniga/desktop/scientificComputing/finite-difference-threading/code/output/timings1thread.txt')
+timings_2_threads = load_timings('/Users/stevenzuniga/desktop/scientificComputing/finite-difference-threading/code/output/timings2thread.txt')
+timings_3_threads = load_timings('/Users/stevenzuniga/desktop/scientificComputing/finite-difference-threading/code/output/timings3thread.txt')
 
 x = np.arange(1, len(timings_1_thread) + 1)
 plt.figure(figsize=(10, 6))
@@ -29,7 +29,7 @@ plt.ylabel('Time (seconds)')
 plt.title('Timings for Different Numbers of Threads')
 plt.legend()
 
-plt.savefig('timings_plot.png')
+plt.savefig('output/timings_plot.png')
 plt.show()
 
 
